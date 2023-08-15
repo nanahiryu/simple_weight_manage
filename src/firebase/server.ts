d188a2a415fd3058a10e4cb0ccdbfd2f376bcc08
nanahiryu <ryu2751x7@gmail.com>
2023-08-15 15:36:47 +0900

    deploy errorの対処

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,11 +1,15 @@
-import { initializeApp, cert, getApps } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
+import { initializeApp, cert, getApps } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 
 if (!getApps()?.length) {
+  if (!process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+    throw new Error('FIREBASE_SERVICE_ACCOUNT_KEY is not set');
+  }
   initializeApp({
     credential: cert(
       // 環境変数から認証情報を取得
-      JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string)
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY),
     ),
   });
 }
