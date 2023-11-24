9b29334043284183a07a2996b6e52a1bfc38f5fd
nanahiryu <ryu2751x7@gmail.com>
2023-11-24 12:24:03 +0900

    トレーニング用ページを整理

--- src/app/training/page.tsx
+++ src/app/training/page.tsx
@@ -1,16 +1,10 @@
 'use client';
 
-import { Flex, Icon, Input, InputGroup, InputLeftElement, Text } from '@chakra-ui/react';
-import { MdSearch } from 'react-icons/md';
-import { IconType } from 'react-icons';
+import { Flex } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 
-import { CardBase } from '@/components/card';
-import { bodyPartSeedList } from '@/seed/bodyParts';
 import { Exercise } from '@/types/exercise';
 import { exerciseSeedList } from '@/seed/exercise';
-
-import ExerciseCard from './_components/ExerciseCard';
 
 const TrainingPage = () => {
   const [searchText, setSearchText] = useState<string>('');
@@ -48,51 +42,7 @@ const TrainingPage = () => {
     return () => clearTimeout(timer);
   }, [searchText, filteredExerciseList]);
 
-  return (
-    <Flex py="60px" px="40px" gap="40px" w="90%" h="full">
-      <Flex direction="column" gap="12px">
-        <InputGroup>
-          <InputLeftElement>
-            <Icon as={MdSearch as IconType} boxSize="24px" />
-          </InputLeftElement>
-          <Input w="280px" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
-        </InputGroup>
-        <CardBase w="280px" minH="200px" h="fit-content" justify="start" direction="column" py="20px" gap="8px">
-          {/* FIXME: bodyPartsのcreate機能を作ったらseedから置き換える */}
-          {bodyPartSeedList.map((content) => (
-            <Flex
-              key={content.id}
-              onClick={() => setSelectedBodyPartId(content.id)}
-              w="full"
-              color={selectedBodyPartId === content.id ? 'teal.400' : 'black'}
-              _hover={{
-                cursor: 'pointer',
-                color: 'teal.300',
-              }}
-            >
-              <Text fontSize="lg" fontWeight="semibold">
-                {content.name}
-              </Text>
-            </Flex>
-          ))}
-        </CardBase>
-      </Flex>
-      <Flex w="70%" h="full" wrap="wrap" gap="20px">
-        {searchedExerciseList.map((exercise) => (
-          <ExerciseCard
-            key={exercise.id}
-            title={exercise.name}
-            imagePath={exercise.imagePath}
-            loadType={exercise.loadType}
-            load={exercise.currentLoad}
-            reps={exercise.currentReps}
-            sets={exercise.currentSets}
-            bodyPartsIdList={exercise.bodyPartsIdList}
-          />
-        ))}
-      </Flex>
-    </Flex>
-  );
+  return <Flex py="60px" px="40px" gap="40px" w="90%" h="full"></Flex>;
 };
 
 export default TrainingPage;
