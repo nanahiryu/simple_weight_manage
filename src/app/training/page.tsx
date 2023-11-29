cc061e2c61e48888123461f215d12f971dd85ebb
nanahiryu <ryu2751x7@gmail.com>
2023-11-29 16:02:09 +0900

    calendarUIの追加

--- src/app/training/page.tsx
+++ src/app/training/page.tsx
@@ -4,6 +4,7 @@ import { Flex, Text } from '@chakra-ui/react';
 import React, { useState } from 'react';
 
 import { YearMonth, YearMonthPaging } from '@/components/paging';
+import Calendar from '@/components/calendar';
 
 const TrainingPage = () => {
   const [selectedYearMonth, setSelectedYearMonth] = useState<YearMonth>({
@@ -12,15 +13,15 @@ const TrainingPage = () => {
   });
 
   return (
-    <Flex py="40px" px="40px" gap="40px" w="90%" h="full" direction="column">
+    <Flex py="40px" px="40px" gap="20px" w="90%" h="full" direction="column">
       <Flex>
         <Text fontSize="2xl" fontWeight="semibold">
           トレーニングログ
         </Text>
       </Flex>
-      <Flex w="full" direction="column" align="center">
+      <Flex w="full" direction="column" align="center" gap="12px">
         <YearMonthPaging yearMonth={selectedYearMonth} setYearMonth={setSelectedYearMonth} />
-        <Flex></Flex>
+        <Calendar yearMonth={selectedYearMonth} />
       </Flex>
     </Flex>
   );
