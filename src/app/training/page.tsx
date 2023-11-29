da0ebe3181362f855c08190d33bdec39c60778a1
nanahiryu <ryu2751x7@gmail.com>
2023-11-29 15:06:31 +0900

    一旦commit

--- src/app/training/page.tsx
+++ src/app/training/page.tsx
@@ -1,10 +1,29 @@
 'use client';
 
-import { Flex } from '@chakra-ui/react';
-import React from 'react';
+import { Flex, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
+
+import { YearMonth, YearMonthPaging } from '@/components/paging';
 
 const TrainingPage = () => {
-  return <Flex py="60px" px="40px" gap="40px" w="90%" h="full"></Flex>;
+  const [selectedYearMonth, setSelectedYearMonth] = useState<YearMonth>({
+    year: new Date().getFullYear(),
+    month: new Date().getMonth() + 1,
+  });
+
+  return (
+    <Flex py="40px" px="40px" gap="40px" w="90%" h="full" direction="column">
+      <Flex>
+        <Text fontSize="2xl" fontWeight="semibold">
+          トレーニングログ
+        </Text>
+      </Flex>
+      <Flex w="full" direction="column" align="center">
+        <YearMonthPaging yearMonth={selectedYearMonth} setYearMonth={setSelectedYearMonth} />
+        <Flex></Flex>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default TrainingPage;
